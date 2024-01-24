66cf41ae42876396ed383d44e0717cbb059eaa21
makerswr <alceoswr@gmail.com>
2024-01-24 14:00:22 -0500

    go back btn fixed

--- App.jsx
+++ App.jsx
@@ -11,7 +11,7 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home" screenOptions={{headerShown:false}}>
+        <Stack.Navigator initialRouteName="Home">
           <Stack.Screen name ="Home" component={Home}/>
           <Stack.Screen name="Login" component={Login}/>
           <Stack.Screen name="Signup" component={Signup}/>
